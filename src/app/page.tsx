32a32d29499c056c6242a7f6eec8594e0af913a3
Park Sooyeon <sooyeon@aniai.ai>
2026-08-19 17:25:42 +0900

    minor spacing fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -140,7 +140,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="max-w-[560px] mx-auto w-full mt-8 sm:mt-12 lg:mt-14">
+        <div className="max-w-[560px] mx-auto w-full mt-16 sm:mt-14 lg:mt-16">
           <div className="flex flex-col gap-3 sm:gap-1 lg:gap-1">
             <ProjectListItem
               title="Robot Interface for Collaboration in Kitchen"
